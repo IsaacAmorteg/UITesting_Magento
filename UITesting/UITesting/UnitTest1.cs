141378aaea80a57283ccd28bca6c48764a115539
Isaac Amortegui <isaacamortegc@outlook.com>
2023-06-14 23:38:27 -0500

    Scenario 1 in progress/checkpoint

--- UITesting/UITesting/UnitTest1.cs
+++ UITesting/UITesting/UnitTest1.cs
@@ -1,7 +1,11 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
+using System.Xml.Linq;
 
 namespace UITesting
 {
@@ -15,7 +19,9 @@ namespace UITesting
         public void Setup()
         {
             _driver = new ChromeDriver();
-           // _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
             _driver.Manage().Window.Maximize();
         }
 
@@ -48,13 +54,15 @@ namespace UITesting
 
             signInButtonLoginPage.Click();
 
-            Thread.Sleep(TimeSpan.FromSeconds(2));
+            var expected = "Welcome, Isaac Amortegui!";
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(1));
+            wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.ClassName("logged-in"), expected));
 
             IWebElement welcomeMessage = _driver.FindElement(By.ClassName("logged-in"));
 
             //Assert
-            var actual = welcomeMessage.Text;
-            var expected = "Welcome, Isaac Amortegui!";
+            var actual = welcomeMessage.Text;            
 
             Assert.AreEqual(expected, actual);
                        
@@ -80,8 +88,6 @@ namespace UITesting
 
             signInButtonLoginPage.Click();
 
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-
             var isSignInButtonDisplayedCollection = _driver.FindElements(By.ClassName("authorization-link")).Select(i => i.Displayed);
 
             //Assert
@@ -95,7 +101,8 @@ namespace UITesting
             _driver.Navigate().GoToUrl(_gearPageUrl);
 
             //Action
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(1));
+            wait.Until(driver => driver.FindElements(By.ClassName("product-item-link")).All(i =>i.Text != string.Empty));
             IEnumerable<IWebElement> productNameCollection = _driver.FindElements(By.ClassName("product-item-link"));
             var actual = productNameCollection.Select(i => i.Text);
             var expected = new[]
@@ -109,14 +116,58 @@ namespace UITesting
             //Assert
             Assert.AreEqual(expected, actual);
         }
-        /*
+        
         [Test]
-        public void Sandbox()
+        public void Scenario1()
         {
+            //Precondition
             _driver.Navigate().GoToUrl(_baseUrl);
-            var value = _driver.FindElements(By.Id("2345353454"));
+
+            //Action
+            IWebElement signInButton = _driver.FindElement(By.XPath(".//li[@class='authorization-link']"));
+            signInButton.Click();
+
+            IWebElement emailInput = _driver.FindElement(By.Id("email"));
+            IWebElement passwordInput = _driver.FindElement(By.Name("login[password]"));
+
+            emailInput.SendKeys("isaacamortegc@outlook.com");
+            passwordInput.SendKeys("Boeing787");
+
+            IWebElement signInButtonLoginPage = _driver.FindElement(By.Id("send2"));
+
+            signInButtonLoginPage.Click();
+
+            IWebElement gearButtonDropDownMenu = _driver.FindElement(By.Id("ui-id-6"));
+
+            Actions actions = new Actions(_driver);
+
+            actions.MoveToElement(gearButtonDropDownMenu).Perform();                 
+
+            IWebElement watchesButtonByGearDropDownMenu = _driver.FindElement(By.XPath(".//a[@id= 'ui-id-27']"));
+            watchesButtonByGearDropDownMenu.Click();
+
+            IWebElement productBox = _driver.FindElement(By.XPath(".//li[@class='item product product-item']//a[contains(text(), 'Dash Digital Watch')]"));
+            actions.MoveToElement(productBox).Build().Perform();
+
+            IWebElement addToCartButton = _driver.FindElement(By.XPath(".//form[@data-product-sku= '24-MG02']/button[@class='action tocart primary']"));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)_driver;
+            jsExecutor.ExecuteScript("arguments[0].click();", addToCartButton);
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            IWebElement showMiniCartCheckoutButton = _driver.FindElement(By.ClassName("minicart-wrapper"));
+            showMiniCartCheckoutButton.Click();
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            IWebElement proceedToCheckoutButton = _driver.FindElement(By.Id("top-cart-btn-checkout"));
+            proceedToCheckoutButton.Click();
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+
+            //cONTINUE WORK HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         }
-        */
+        
 
         [TearDown]
         public void Teardown()

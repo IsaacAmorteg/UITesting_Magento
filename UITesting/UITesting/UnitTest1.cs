6d9894e26c60c0092bb0c0d0eed7c76ad3eb579d
Isaac Amortegui <isaacamortegc@outlook.com>
2023-06-14 21:31:17 -0500

    Sandbox test added

--- UITesting/UITesting/UnitTest1.cs
+++ UITesting/UITesting/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 namespace UITesting
 {
@@ -8,11 +9,13 @@ namespace UITesting
     {
         private IWebDriver _driver;
         private readonly string _baseUrl = "https://magento.softwaretestingboard.com/";
+        private readonly string _gearPageUrl = "https://magento.softwaretestingboard.com/gear.html";
 
         [SetUp]
         public void Setup()
         {
             _driver = new ChromeDriver();
+           // _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
             _driver.Manage().Window.Maximize();
         }
 
@@ -26,7 +29,6 @@ namespace UITesting
 
         }
         [Test]
-
         public void BasePageOpened_SignIn_WelcomeMessageIsDisplayed()
         {
             //Precondition
@@ -46,7 +48,7 @@ namespace UITesting
 
             signInButtonLoginPage.Click();
 
-            Thread.Sleep(TimeSpan.FromSeconds(3));
+            Thread.Sleep(TimeSpan.FromSeconds(2));
 
             IWebElement welcomeMessage = _driver.FindElement(By.ClassName("logged-in"));
 
@@ -55,8 +57,66 @@ namespace UITesting
             var expected = "Welcome, Isaac Amortegui!";
 
             Assert.AreEqual(expected, actual);
+                       
+        }
+
+        [Test]
+        public void BasePageOpened_SignIn_MainPageNotSignInButton()
+        {
+            //Precondition
+            _driver.Navigate().GoToUrl(_baseUrl);
+
+            //Action
+            IWebElement signInButton = _driver.FindElement(By.XPath(".//li[@class='authorization-link']"));
+            signInButton.Click();
+
+            IWebElement emailInput = _driver.FindElement(By.Id("email"));
+            IWebElement passwordInput = _driver.FindElement(By.Name("login[password]"));
+
+            emailInput.SendKeys("isaacamortegc@outlook.com");
+            passwordInput.SendKeys("Boeing787");
+
+            IWebElement signInButtonLoginPage = _driver.FindElement(By.Id("send2"));
+
+            signInButtonLoginPage.Click();
+
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var isSignInButtonDisplayedCollection = _driver.FindElements(By.ClassName("authorization-link")).Select(i => i.Displayed);
+
+            //Assert
+            Assert.IsFalse(isSignInButtonDisplayedCollection.Distinct().Single());                             
 
         }
+        [Test]
+        public void OpenGearPage_GetHotSellers_ProductsAreCorrect()
+        {
+            //Precondition
+            _driver.Navigate().GoToUrl(_gearPageUrl);
+
+            //Action
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            IEnumerable<IWebElement> productNameCollection = _driver.FindElements(By.ClassName("product-item-link"));
+            var actual = productNameCollection.Select(i => i.Text);
+            var expected = new[]
+            {
+                "Fusion Backpack",
+                "Push It Messenger Bag",
+                "Affirm Water Bottle",
+                "Sprite Yoga Companion Kit"
+            };
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /*
+        [Test]
+        public void Sandbox()
+        {
+            _driver.Navigate().GoToUrl(_baseUrl);
+            var value = _driver.FindElements(By.Id("2345353454"));
+        }
+        */
 
         [TearDown]
         public void Teardown()
